25496ed408540718d4488163fceb4ef7f3ed73ef
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2024-12-04 10:15:54 -0500

    Add files via upload

--- DifferenceBtwnListsRunner.java
+++ DifferenceBtwnListsRunner.java
@@ -1,17 +1,23 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
+import java.util.Scanner;
 
 public class DifferenceBtwnListsRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File differenceBetweenListsNumbers = new File("DifferenceBtwnListsNumbers");
+        Scanner myReader = new Scanner(differenceBetweenListsNumbers);
         List<Integer> listOne = new ArrayList<>();
         List<Integer> listTwo = new ArrayList<>();
         int listOneOrTwo = 0;
 
-
         for (int b = 1; b <= 10000; b++){
-
+            String data = myReader.nextLine();
+            String[] dataByLetters = data.split("");
+            for (int c = 0; c <= 4; c++){
+//                listOne.add(Integer.parseInt(dataByLetters.get))
+            }
         }
     }
 
