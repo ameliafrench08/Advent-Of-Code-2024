61fe75822ac4a1f6652cfb032a52f974cdbcda4b
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2024-12-06 10:52:46 -0500

    Add files via upload

--- SafetyReports.java
+++ SafetyReports.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SafetyReports {
+    private List<Integer> report = new ArrayList<>();
+    private boolean isSafe;
+
+    public SafetyReports(List<Integer> report){
+        this.report = report;
+    }
+
+    public boolean isIncreasing(){
+        int numTimesIncreasing = 0;
+        for (int i = 0; i < report.size(); i++){
+            if (report.size() - i >= 2 && (report.get(i) < report.get(i +1))){
+                numTimesIncreasing++;
+            }
+        }
+        return numTimesIncreasing + 1 == report.size();
+    }
+
+    public boolean isDecreasing(){
+        int numTimesDecreasing = 0;
+        for (int l = 0; l < report.size(); l++){
+            if (report.size() - l >= 2 && (report.get(l) > report.get(l +1))){
+                numTimesDecreasing++;
+            }
+        }
+        return numTimesDecreasing + 1 == report.size();
+    }
+
+    public boolean differencesSafe(){
+        for (int k = 0; k < report.size(); k++){
+            int difference = 0;
+            if (report.size() - k >= 2) {
+                difference = Math.abs(report.get(k) - report.get(k + 1));
+            }
+            if (difference >= 4 || difference < 1){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- SafetyReportsRunner.java
+++ SafetyReportsRunner.java
@@ -1,5 +1,38 @@
-public class SafetyReportsRunner {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
+import static java.awt.AWTEventMulticaster.remove;
+
+public class SafetyReportsRunner {
+    public static void main(String[] args) throws FileNotFoundException {
+        File SafetyReportsNumbers = new File("C:\\Users\\BT_2S14_05\\IdeaProjects\\AdventOfCode_AmeliaFrench\\src\\SafetyReportsData");
+        Scanner safetyReportsScanner = new Scanner(SafetyReportsNumbers);
+
+        int numSafe = 0;
+        while (safetyReportsScanner.hasNext()){
+            List<Integer> report = new ArrayList<>();
+            String data = safetyReportsScanner.nextLine();
+            String[] dataByLetters = data.split("");
+            String number = "";
+
+            for (String dataByLetter : dataByLetters) {
+                if (!Objects.equals(dataByLetter, " ")) {
+                    number += dataByLetter;
+                } else {
+                    report.add(Integer.parseInt(number));
+                    number = "";
+                }
+            }
+            SafetyReports safetyReport = new SafetyReports(report);
+            if ((safetyReport.isDecreasing() || safetyReport.isIncreasing()) && safetyReport.differencesSafe()){
+                numSafe++;
+            }
+        }
+        System.out.println("Num safe: ");
+        System.out.println(numSafe);
     }
 }
